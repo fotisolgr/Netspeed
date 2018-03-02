cc8f8711b3720a7b2ae5d1546b947eed3f9ab095
Fotis Soldatos <fotisolgr@gmail.com>
2018-03-02 08:59:42 +0200

    Update speedworker-post.js

--- speedworker/speedworker-post.js
+++ speedworker/speedworker-post.js
@@ -15,15 +15,9 @@ var application = "speedtest_worker";
 var device = "";
 
 var agent_ip = document.getElementById("settings").getAttribute("agentIp");
+var agent_port = document.getElementById("settings").getAttribute("agentPort");
 var interval = setInterval(function(){w.postMessage("status");}.bind(this),100); //ask for status every 100ms
 //--------------------------------------------------------------------------------------------
-
- w.onmessage=function(event){ 
-
-	var data=event.data.split(";"); //(status: 0=not started, 1=downloading, 2=uploading, 3=ping, 4=done, 5=aborted)
- 	var status=Number(data[0]);
-
-//------------------------------------------------------
 // Default xronos gia to cookie_time
 
 if (typeof document.getElementById("settings").getAttribute("cookieTimeInMinutes") === 'undefined' || document.getElementById("settings").getAttribute("cookieTimeInMinutes") == '') {
@@ -33,30 +27,64 @@ if (typeof document.getElementById("settings").getAttribute("cookieTimeInMinutes
 }
 //------------------------------------------------------
 //Kathorismos http protocol
-if (typeof document.getElementById("settings").getAttribute("hostingWebsite") === 'undefined' || document.getElementById("settings").getAttribute("hostingWebsite") == null || document.getElementById("settings").getAttribute("hostingWebsite") == '' || document.getElementById("settings").getAttribute("hostingWebsite") == 'https') {
-	var agent = "https://" + agent_ip + ":8443/wifimon/";
+
+if (document.getElementById("settings").getAttribute("hostingWebsite") == 'http') {
+	var agent = "http://" + agent_ip + ":" + agent_port+ "/wifimon/";
 }else{
-	var agent = "http://" + agent_ip + ":9000/wifimon/";
+	var agent = "https://" + agent_ip + ":" + agent_port+ "/wifimon/";
+}
+//-----------------------------------------------------------------------------------------------------------------
+// Elegxos gia ergaleio metrisis
+if (typeof document.getElementById("settings").getAttribute("testtool") === 'undefined' || document.getElementById("settings").getAttribute("testtool") == '') {
+	var test_tool = "N/A";
+}else{
+	var test_tool = document.getElementById("settings").getAttribute("testtool");
 }
 //------------------------------------------------------
-                
-	//document.getElementById("download").innerHTML=data[1]+" Mbit/s";
-                
-	//document.getElementById("upload").innerHTML=data[2]+" Mbit/s";
-                
-	//document.getElementById("ping").innerHTML=data[3]+" ms ";
-                
-		                
-          
+//Set and check cookie 
+
+function setCookie(cname,cvalue,exhours) {
+ var d = new Date();
+ d.setTime(d.getTime() + (exhours*60*60*1000));
+ var expires = "expires=" + d.toGMTString();
+ document.cookie = cname+"="+cvalue+"; "+expires;
+} 
+//------------------------------------------------------
+//Get cookie
+ 
+function getCookie(cname) {
+ var name = cname + "=";
+ var ca = document.cookie.split(';');
+ for(var i=0; i<ca.length; i++) {
+  var c = ca[i];
+  while (c.charAt(0)==' ') c = c.substring(1);
+  if (c.indexOf(name) == 0) {
+   return c.substring(name.length, c.length);
+  }
+ }
+ return "";
+} 
+//------------------------------------------------------
+// Check Cookie
+
+function checkCookie() {
+ var checkTest=getCookie("Speedtest");
+ if (checkTest != "") {
+ } else {
+    setCookie("Speedtest", "Test Already Performed", cookie_time/60);
+     w.onmessage=function(event){ 
+
+	var data=event.data.split(";"); //(status: 0=not started, 1=downloading, 2=uploading, 3=ping, 4=done, 5=aborted)
+ 	var status=Number(data[0]);
 
                     download_throughputMb = data[1];
  		    upload_throughputMb = data[2];
                     local_ping = data[3];
 
 //Metatropi se KB/s
-download_throughput = (Math.round((download_throughputMb*125) * 10) / 10).toFixed(0)
-upload_throughput = (Math.round((upload_throughputMb*125) * 10) / 10).toFixed(0)
-local_ping = (Math.round((local_ping) * 10) / 10).toFixed(0)
+	download_throughput = (Math.round((download_throughputMb*125) * 10) / 10).toFixed(0)
+	upload_throughput = (Math.round((upload_throughputMb*125) * 10) / 10).toFixed(0)
+	local_ping = (Math.round((local_ping) * 10) / 10).toFixed(0)
 
 
  if(status >= 4 ){ // Otan status=4 to test exei oloklirwthei.
@@ -113,18 +141,16 @@ else {
 }
 //------------------------------------------------------
 detectDevice(); //call tou detectDevice
+geoTest();
 //------------------------------------------------------
-
-postToAgent(local_ping,download_throughput,upload_throughput,device,application); //call tou postToAgent
+postToAgent(local_ping,download_throughput,upload_throughput,latitude,longitude,location_method,device,application); //call tou postToAgent
 //------------------------------------------------------
 // Post measurement to agent
 
-
-function postToAgent(local_ping,download_throughput,upload_throughput,device,application) {
+function postToAgent(local_ping,download_throughput,upload_throughput,latitude,longitude,location_method,device,application) {
  if (typeof download_throughput === 'undefined' || isNaN(download_throughput) || !download_throughput) {
   download_throughput = 0;
  }
-
  if (typeof upload_throughput === 'undefined' || isNaN(upload_throughput) || !upload_throughput) {
   upload_throughput = 0;
  }
@@ -138,63 +164,29 @@ function postToAgent(local_ping,download_throughput,upload_throughput,device,app
  if (typeof location_method === 'undefined') {
   location_method = "N/A";
  }
-console.log("Download speed is :", download_throughput);
-console.log("Upload speed is :", upload_throughput);
-console.log("Local ping is :", local_ping);	
-console.log("Device :", device);
-console.log("Application :", application);
 
 var measurement = {downloadThroughput: download_throughput,
                    uploadThroughput: upload_throughput,
                    localPing: local_ping,
                    latitude: latitude,
                    longitude: longitude,
-                   locationMethod: location_method};
+                   locationMethod: location_method,
+				   testTool: test_tool};
 
-console.log(measurement);
+
 $.ajax({
 	type: "POST",
 	data :JSON.stringify(measurement),
 	url: agent + "add/",
 	contentType: "application/json"
 });
-
 }//telos postToAgent
-//------------------------------------------------------
-//Set and check cookie 
+}//telos if (status >= 4)
 
-function setCookie(cname,cvalue,exhours) {
- var d = new Date();
- d.setTime(d.getTime() + (exhours*60*60*1000));
- var expires = "expires=" + d.toGMTString();
- document.cookie = cname+"="+cvalue+"; "+expires;
-} 
-//------------------------------------------------------
-//Get cookie
- 
-function getCookie(cname) {
- var name = cname + "=";
- var ca = document.cookie.split(';');
- for(var i=0; i<ca.length; i++) {
-  var c = ca[i];
-  while (c.charAt(0)==' ') c = c.substring(1);
-  if (c.indexOf(name) == 0) {
-   return c.substring(name.length, c.length);
-  }
- }
- return "";
-} 
-//------------------------------------------------------
-// Check Cookie
-
-function checkCookie() {
- var checkTest=getCookie("Boomerang");
- if (checkTest != "") {
- } else {
-    setCookie("Boomerang", "Test Already Performed", cookie_time/60);
-   
- }
-}
+}//telos function(event){}
+w.postMessage('start {"time_dl":"5", "time_ul":"5", "count_ping":"5", "garbagePhp_chunkSize":"10"}'); //enarksi tou test me tis sygkekrimenes parametrous. 
+ }//telos else tou checkCookie
+}//telos function checkCookie
 //------------------------------------------------------
 //Run checkCookie function
 
@@ -215,8 +207,3 @@ function runCheckCookie() {
 }
 //------------------------------------------------------
 runCheckCookie(); // call tou runCheckCookie function
-
-}//telos if (status >= 4)
-
-}//telos function(event){}
-w.postMessage('start {"time_dl":"5", "time_ul":"5", "count_ping":"5", "garbagePhp_chunkSize":"10"}'); //enarksi tou test me tis sygkekrimenes parametrous.
